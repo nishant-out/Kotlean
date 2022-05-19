3efd59d7bad68c9d05578213324d656c76d897fd
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-05-19 20:50:32 +0530

    Create var_and_val.kt

---
+++ var_and_val.kt
@@ -0,0 +1,18 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+ 
+fun main(){ 
+    
+    val str: String = "hello "	//	Immutable i.e. str = "newString"	//	Error
+    var str2 : String = "new String"
+    
+    println( str2 )
+    
+    str2 = "new2"
+    
+    var i = 5
+    
+    println( "Kotin " + str + i )
+}
