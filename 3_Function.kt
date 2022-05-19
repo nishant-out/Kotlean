61648701f55f70a77ace52a3773d7e3ea9562b05
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-05-19 23:00:54 +0530

    Create 3_Function.kt

---
+++ 3_Function.kt
@@ -0,0 +1,29 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+/*
+fun function_name( arg1: type, arg2........ )
+ */
+
+fun say( ){
+    println( "Hello" )
+}
+
+// Or
+
+fun say2( ) = println( "Hello2" )
+
+
+//	Or
+
+fun say3( arg: String ){
+    println( "HEllo $arg" )
+}
+
+
+fun main(){ 
+    
+    say3( "Nishant" )
+}
